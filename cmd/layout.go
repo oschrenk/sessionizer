d9af35137320e03771e14d85ee4fda4fbd2c34d7
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-12-13 14:36:08 -0600

    :art: Call apply

--- cmd/layout.go
+++ cmd/layout.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/oschrenk/sessionizer/core"
 	"github.com/oschrenk/sessionizer/internal/tmuxp"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
@@ -11,6 +12,7 @@ import (
 
 func init() {
 	rootCmd.AddCommand(layoutCmd)
+	layoutCmd.Flags().BoolP("apply", "a", false, "Apply the layout")
 }
 
 var layoutCmd = &cobra.Command{
@@ -20,11 +22,22 @@ var layoutCmd = &cobra.Command{
 	Hidden: true,
 	Run: func(cmd *cobra.Command, args []string) {
 		filePath := args[0]
+		apply, _ := cmd.Flags().GetBool("apply")
 
 		layout, err := tmuxp.ReadLayoutFromFile(filePath)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error reading layout file: %v\n", err)
 			os.Exit(1)
+		}
+
+		if apply {
+			err := core.ApplyLayout(layout)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error applying layout: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Println("Applied")
+			return
 		}
 
 		yamlData, err := yaml.Marshal(layout)
---
+++ core/layout.go
@@ -0,0 +1,11 @@
+package core
+
+import (
+	"github.com/oschrenk/sessionizer/internal/tmuxp"
+)
+
+// ApplyLayout applies a tmuxp layout configuration
+func ApplyLayout(layout *tmuxp.Layout) error {
+	// TODO: Implement layout application logic
+	return nil
+}
